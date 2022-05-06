a732dbe1592aeb686417c21f746c5e0f653f2a28
Thiago Cruz <morim24093@gmail.com>
2022-05-06 18:30:28 -0300

    create on

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 const express = require('express');
+const {v4: uuidv4} =  require('uuid')
 
 const app = express();
+
+app.use(express.json())
+
+const costumers = [];
+
+
+/* metodos
+cpf - string
+name - string
+id - uuid
+statement []
+*/
+
+app.post('/account', (req, res) => {
+  const {cpf, name} = req.body;
+
+  const id = uuidv4();
+
+  costumers.push({
+    cpf,
+    name,
+    id,
+    statement: []
+  });
+  return res.status(201).send();
+})
 
 app.listen(3333);
