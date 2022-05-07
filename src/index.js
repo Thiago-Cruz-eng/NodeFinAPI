f7ba8431440eb9b9ee482a93c3dce243ddb76c02
Thiago Cruz <morim24093@gmail.com>
2022-05-07 07:21:01 -0300

    using middleware

--- src/index.js
+++ src/index.js
@@ -7,13 +7,20 @@ app.use(express.json())
 
 const customers = [];
 
+//middleware for verify cpf to not read myself many times in the functions
+function verifyExistsAcoountCPF(req, res, next) {
+//take a cpf from my url, like params and with this using by destructuring
+  const {cpf} = req.headers;
+//find != some - some is true or false, finda return all data if find
+  const customer = customers.find(customer => customer.cpf === cpf);
 
-/* metodos
-cpf - string
-name - string
-id - uuid
-statement []
-*/
+  if(!customer){
+    return res.status(400).json({error: 'Customer not found!'});
+  }
+
+  req.customer = customer;
+  return next();
+}
 
 app.post('/account', (req, res) => {
   const {cpf, name} = req.body;
@@ -33,6 +40,13 @@ app.post('/account', (req, res) => {
     statement: []
   });
   return res.status(201).send();
-})
+});
+
+app.get("/statement", verifyExistsAcoountCPF, (req, res) => {
+//take-destructuring customer of my req in middleware
+  const {customer} = req;
+//here is like, I wil take my client, all of data, but I only will response with statement
+  return res.json(customer.statement);
+});
 
 app.listen(3333);
